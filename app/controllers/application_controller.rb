ee311319c872c77b3ae97f24dabe091c11683d4d
EC2 Default User <ec2-user@ip-172-31-30-52.ec2.internal>
2021-10-13 13:33:49 +0000

    [add]管理者ログイン、ジャンル画面追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_daleted])
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      admin_items_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin_items_path
+      new_admin_session_path
+    else
+      new_customer_session_path
+    end
+  end
+
+  def customer_shut_out
+    unless admin_signed_in?
+      redirect_to top_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,41 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :customer,
+  path: :customers,
+    :controllers => {
+      :registrations => "customer/registrations",
+      :sessions => "customer/sessions",
+      :passwords => "customer/passwords"
+    }
+
+  devise_for :admin,
+  path: :admins,
+  :controllers => {
+    :sessions => "admin/sessions"
+  }
+
+  get "/" => "customer/item#top", as: "top"
+  get "/about" => "customer/item#about", as: "about"
+  get "/customers" => "customer/customers#rule", as: "customer_rule"
+
+  namespace :admin, path: :admins do
+    resources :items
+    resources :genres
+    resources :orders
+    resources :orders_datails
+    resources :customers
+    get "orders/current_user_order/:id" => "orders#current_user_order"
+  end
+
+  namespace :customer, path: :customers do
+    resources :items
+    resources :cart_items
+    delete "cart_items" => "cart_items#destroy_all", as: "cart_item_destroy_all"
+
+    resources :orders do
+    collection do
+      post "confirm"
+      get "conplete"
+    end
+  end
+end
 end
